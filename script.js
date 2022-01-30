b0dbce9b40e6f83c60243047df3bd026cf4beaf1
trunovandrey <trunovandrey2104@gmail.com>
2022-01-30 21:31:56 +0300

    homework

---
+++ script.js
@@ -0,0 +1,43 @@
+                                    //Задание №1
+
+// let firstNumber = Number( prompt('Напишите число №1'));
+// let secondNumber = Number (prompt('Напишите число №2'));
+
+// if (firstNumber>secondNumber){
+//     alert( ` Число ${firstNumber} больше числа ${secondNumber} `);
+// } else if (firstNumber<secondNumber){
+//     alert( ` Число ${firstNumber} меньше числа ${secondNumber} `);
+// } else if (firstNumber===secondNumber){
+//     alert('Числа равны!');
+// } else {
+//     alert('Такого числа нет');
+// }
+
+                                       //Задание №2
+
+// let firstNumber = Number( prompt('Напишите число №1'));
+// let secondNumber = Number (prompt('Напишите число №2'));
+// let action = prompt('Выберите действие (+ , - , / , *)');
+
+// let result = (firstNumber + action + secondNumber);
+// alert(result);
+
+                                        //Задание №3
+
+// let userAge = Number(prompt(' Укажите свой возрост')); 
+// let minAge = 18 ;
+// let maxAge = 60;
+// let smallAge = 7;
+
+// if (userAge > maxAge){
+//     alert('Вам вход запрещен!!!')
+// }else if (userAge<smallAge){
+//     alert('Вы слишком малы')
+// }else if(userAge > minAge){
+//     alert('Добро пожаловать!')
+// } else if(userAge <= minAge){
+//    confirm('Вам разрешили родители?')
+   
+// } else if (userAge > maxAge){
+//     alert('Вам вход запрещен!!!')
+// }
